23111202a023638db4c3978be419922524eb359f
Marcus Fonseca <mv070900@gmail.com>
2025-10-25 01:24:19 -0300

    começo do preenchimento das entidades

--- clinic/src/main/java/com/ifba/clinic/model/entity/Doctor.java
+++ clinic/src/main/java/com/ifba/clinic/model/entity/Doctor.java
@@ -1,17 +1,43 @@
 package com.ifba.clinic.model.entity;
 
 import com.ifba.clinic.model.enums.Speciality;
+import com.ifba.clinic.model.vo.Crm;
 
-import jakarta.persistence.Entity;
+import com.ifba.clinic.model.vo.PhoneNumber;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
     private String name;
+
+    @NotBlank
+    @Email
     private String email;
-    private String phone;
-    private String crm;
+
+    @NotNull
+    @Valid
+    private PhoneNumber phone;
+
+    @NotNull
+    @Valid
+    private Crm crm;
+
+    @NotBlank
+    @Enumerated(EnumType.STRING)
     private Speciality speciality;
+
+    @NotNull
+    @Valid
     private Address adress;
-    
+    private Boolean active = true;
 }
--- clinic/src/main/java/com/ifba/clinic/model/vo/Crm.java
+++ clinic/src/main/java/com/ifba/clinic/model/vo/Crm.java
@@ -2,4 +2,5 @@ package com.ifba.clinic.model.vo;
 
 public class Crm {
 
+    private final String crm;
 }
