bef8b089b7ae8dff24c5fbe58b328637fe295c6a
Marcus Fonseca <mv070900@gmail.com>
2025-10-28 18:27:57 -0300

    feat: created repositories

---
+++ clinic/src/main/java/com/ifba/clinic/model/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.ifba.clinic.model.repository;
+
+import com.ifba.clinic.model.entity.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
