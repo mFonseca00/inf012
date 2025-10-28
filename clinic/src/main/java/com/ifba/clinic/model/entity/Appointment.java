d8c421b09e9170042a415d2cbfd65ec98238b499
Marcus Fonseca <mv070900@gmail.com>
2025-10-28 14:07:46 -0300

    remoção de @Valid

--- clinic/src/main/java/com/ifba/clinic/model/entity/Appointment.java
+++ clinic/src/main/java/com/ifba/clinic/model/entity/Appointment.java
@@ -14,7 +14,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -30,13 +29,11 @@ public class Appointment {
     private Long id;
 
     @NotNull
-    @Valid
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "patient_id", nullable = false)
     private Patient patient;
 
     @NotNull
-    @Valid
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "doctor_id", nullable = false)
     private Doctor doctor;
