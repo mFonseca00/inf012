c0b2804044338555f2dc78f0765be881ca6da979
Marcus Fonseca <mv070900@gmail.com>
2025-11-06 20:44:52 -0300

    criado projeto agenda

---
+++ agenda/src/main/java/com/api/agenda/model/entity/Role.java
@@ -0,0 +1,16 @@
+package com.api.agenda.model.entity;
+
+import com.api.agenda.model.enums.UserRole;
+import jakarta.persistence.*;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+}
