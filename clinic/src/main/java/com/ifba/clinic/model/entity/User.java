5971789673dadd05443ad4172a76b2c46a81df0d
Marcus Fonseca <mv070900@gmail.com>
2025-11-06 21:51:35 -0300

    feat: user and role ajusts

--- clinic/src/main/java/com/ifba/clinic/model/entity/User.java
+++ clinic/src/main/java/com/ifba/clinic/model/entity/User.java
@@ -2,12 +2,13 @@ package com.ifba.clinic.model.entity;
 
 import com.ifba.clinic.model.enums.UserRole;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
