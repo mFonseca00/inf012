d17dfaa4f4d7434553ced502556a358f7f2d69c1
Marcus Fonseca <mv070900@gmail.com>
2025-11-20 10:38:14 -0300

    removed agenda

--- agenda/src/main/java/com/api/agenda/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.agenda.repository;
-
-import com.api.agenda.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    UserDetails findByUsername(String username);
-}
