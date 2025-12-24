cb4ef44b0ae4ec3ca099f6b27aa4f5586e3ba8a0
Marcus Fonseca <mv070900@gmail.com>
2025-12-23 21:23:20 -0300

    Feat: implement address registration and retrieval functionalities in AddressService and enhance DoctorService with AddressService dependency

--- clinic/src/main/java/com/ifba/clinic/service/DoctorService.java
+++ clinic/src/main/java/com/ifba/clinic/service/DoctorService.java
@@ -8,8 +8,13 @@ import com.ifba.clinic.repository.DoctorRepository;
 public class DoctorService {
 
     private final DoctorRepository doctorRepository;
+    private final AddressService addressService;
 
-    DoctorService(DoctorRepository doctorRepository){
+    DoctorService(DoctorRepository doctorRepository, AddressService addressService){
         this.doctorRepository = doctorRepository;
+        this.addressService = addressService;
     }
+
+    // Para manipulação de doctor, chamar service de address para manipulação do address
+
 }
