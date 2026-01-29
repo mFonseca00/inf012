e41ea58f2ebaf348db6d8f928f11ac0689be77ea
Iuri <iurii.vianna@gmail.com>
2026-01-29 17:40:36 -0300

    Concluded

--- backend/clinic/src/main/java/com/ifba/clinic/service/TokenService.java
+++ backend/clinic/src/main/java/com/ifba/clinic/service/TokenService.java
@@ -1,15 +1,16 @@
 package com.ifba.clinic.service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.ifba.clinic.model.entity.User;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
